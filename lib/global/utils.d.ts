466cc506ab6e5b358445f8203f4209ce7d56ab53
yuyiRai <yuyi919606952@foxmail.com>
2019-05-19 22:31:03 +0800

    'NEXT'

--- lib/global/utils.d.ts
+++ lib/global/utils.d.ts
@@ -1,13 +1,7 @@
 import { IUtils } from '../utils/Utils';
-import { keys as Keys } from 'ts-transformer-keys';
-import { oc as Oc } from 'ts-optchain';
 declare global {
     var Utils: IUtils;
-    var keys: typeof Keys;
-    var oc: typeof Oc;
     interface Window {
         Utils: IUtils;
-        keys: typeof Keys;
-        oc: typeof Oc;
     }
 }
