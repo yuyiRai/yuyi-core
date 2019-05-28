415dd12b556688fc40429f7c32cfcd5a9a1b660b
yuyiRai <yuyi919606952@foxmail.com>
2019-05-29 00:01:04 +0800

    'NEXT'

--- package/yuyi-core-night/example/src/index.js
+++ package/yuyi-core-night/example/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader'
 
 import './index.css'
 import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+const Component = hot(module)(App)
+ReactDOM.render(<Component />, document.getElementById('root'))
