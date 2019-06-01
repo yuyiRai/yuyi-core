73edf55b84f6f7be8970d2490692852983d7521a
yuyiRai <yuyi919606952@foxmail.com>
2019-06-01 16:02:20 +0800

    NEXT

---
+++ scripts/commit.js
@@ -0,0 +1,7 @@
+const { cd, exec, echo, touch } = require("shelljs")
+
+
+exec('git add .')
+exec('git commit -a -m "NEXT"')
+exec('git push --set-upstream origin master')
+exec('npm version patch && npm publish')
