7ccd2d0c9b4c06ae91e846002d32740b106978e0
yuyiRai <yuyi919606952@foxmail.com>
2019-06-01 17:17:54 +0800

    NEXT

--- package/yuyi-core-env/config/run-ts.js
+++ package/yuyi-core-env/config/run-ts.js
@@ -1,8 +1,11 @@
 const ts = require('ts-node');
 const path = require('path');
+const fs = require('fs');
+
+const configPath = path.resolve('tsconfig.json')
 
 ts.register({
-  project: path.resolve('tsconfig.json'),
+  project: fs.existsSync(configPath) && configPath,
   typeCheck: false,
   compiler: 'typescript',
   transpileOnly: true,
--- scripts/commit.js
+++
@@ -1,6 +0,0 @@
-const { cd, exec, echo, touch } = require("shelljs")
-
-exec('git add .')
-exec('git commit -a -m "NEXT"')
-exec('git push --set-upstream origin master')
-exec('npm version patch && npm publish')
