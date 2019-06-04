5bcd699c7d462c8e0bad285d3c332d6ed04c20fd
yuyiRai <yuyi919606952@foxmail.com>
2019-06-04 19:56:50 +0800

    NEXT

--- package/yuyi-core-env/config/run-ts.js
+++ package/yuyi-core-env/config/run-ts.js
@@ -1,11 +1,13 @@
 const ts = require('ts-node');
 const path = require('path');
 const fs = require('fs');
+const colors = require('colors')
 
 const configPath = path.resolve('tsconfig.json')
-
+const tsconfig = fs.existsSync(configPath) ? configPath : path.join(__dirname, '../tsconfig.json')
+console.log(colors.cyan('loading tsconfig file: ') + colors.yellow(tsconfig))
 ts.register({
-  project: fs.existsSync(configPath) ? configPath : path.join(__dirname, '../tsconfig.json'),
+  project: tsconfig,
   typeCheck: false,
   compiler: 'typescript',
   transpileOnly: true,
