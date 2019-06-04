edf385f31c74cbcace1f1d33d90ac05b5b241e89
yuyiRai <yuyi919606952@foxmail.com>
2019-06-04 19:19:58 +0800

    NEXT

--- package/yuyi-core-env/.bin/yuyienv.bat
+++ package/yuyi-core-env/.bin/yuyienv.bat
@@ -1,3 +1,3 @@
 @echo off
 
-node -r ../yuyi-core-env/config/run-ts.js ../yuyi-core-env/scripts/commit.ts %1 %2
+node -r ./node_modules/yuyi-core-env/config/run-ts.js ./node_modules/yuyi-core-env/scripts/commit.ts %1 %2
