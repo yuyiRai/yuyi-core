fd81543b186d7090352fbb59429fa33fecab2442
yuyiRai <yuyi919606952@foxmail.com>
2019-10-22 10:21:09 +0800

    NEXT

--- packages/cli/.bin/yuyi.bat
+++ packages/cli/.bin/yuyi.bat
@@ -1,5 +1,5 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" --max_old_space_size=2096 "%~dp0\..\react-scripts\bin\react-scripts.js" %*
+  "%~dp0\node.exe" --max_old_space_size=2096 -r "%~dp0\..\@yuyi\cli\config\run-ts.js" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
