3aa640b8ca99b974466027fa920555d0868404cf
yuyiRai <yuyi919606952@foxmail.com>
2019-10-22 12:22:28 +0800

    NEXT

--- packages/cli/scripts/cli.ts
+++ packages/cli/scripts/cli.ts
@@ -1,27 +1,33 @@
 import { Args } from '@yuyi919/utils/src/NodeUtils';
 import colors from 'colors';
+import fs from 'fs-extra'
+import path from 'path'
+
 try {
-const args = new Args({
-  version: "0.0.1",
-  description: `
-  `
-})
-args.addArgument(['-c', '--config'], { action: 'storeTrue', dest: 'config' })
-const sub = args.parser.addSubparsers({ title: 'exec', help: 'exec *.ts file' })
-const subparser = sub.addParser("exec", { })
-subparser.addArgument('<file>', {
-  action: 'store',
-  type: 'string'
-})
-subparser.addArgument(['-v', '-version'], {
-  action: 'storeTrue',
-  dest: "showVersion"
-})
+  const args = new Args({
+    version: "0.0.1",
+    description: `
+    `
+  })
+  args.addArgument(['-c', '--config'], { action: 'storeTrue', dest: 'config' })
+  const sub = args.parser.addSubparsers({ title: 'actions', description: 'anothers' })
+  const subparser = sub.addParser("exec", { help: 'exec *.ts file' })
+  subparser.addArgument('<file>', {
+    action: 'store',
+    type: 'string'
+  })
+  subparser.addArgument(['-v', '-version'], {
+    action: 'storeTrue',
+    dest: "showVersion"
+  })
   const [r, other] = args.parser.parseKnownArgs()
-  console.log(r, other)
+  // console.log(r, other)
+  const filePath = r['<file>'] && path.join(process.cwd(), r['<file>'])
+  if (filePath && fs.pathExistsSync(filePath)) {
+    console.log(colors.cyan('require file ' + filePath))
+    require(filePath)
+  }
 } catch (error) {
   
 }
 
-
-console.log(colors.cyan('run git commit -a -m "NEXT"'))
