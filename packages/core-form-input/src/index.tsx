491cfb245dc7ed0800abccf23857c59438f60a71
yuyiRai <yuyi919606952@foxmail.com>
2019-11-05 15:38:38 +0800

    v0.1.3

---
+++ packages/core-form-input/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import VirtualizedList from './components/List';
+
+// Delete me
+export const Thing = () => {
+  return (
+    <div>
+      the snozzberries taste like snozzberrie
+      <VirtualizedList></VirtualizedList>
+    </div>
+  );
+};
