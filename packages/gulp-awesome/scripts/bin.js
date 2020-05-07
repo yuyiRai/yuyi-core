72c14a6ee8b3b5397513cf95f8ceaf1ebd2c41a9
yuyiRai <yuyi919606952@foxmail.com>
2020-05-07 16:09:19 +0800

    NEXT

--- packages/gulp-awesome/scripts/bin.js
+++ packages/gulp-awesome/scripts/bin.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+'use strict';
+
 const colors = require('colors')
 const path = require('path')
 const args = process.argv.filter(i => i.indexOf('\\shelljs') === -1)
@@ -8,8 +10,9 @@ if (args.length === 2) {
 
 // const cmd = `gulp --require @yuyi919/env/config/run-ts.js --cwd . ${args.join(' ')}`
 process.argv = args.concat(["--require", require.resolve("@yuyi919/env/config/run-ts.js"), "--cwd", process.cwd()])
-console.log(process.argv)
+// console.log(process.argv)
 console.log("gulp " + process.argv.slice(2).map(
   (arg, i, arr) => i + 4 >= arr.length ? colors.cyan(arg) : arg
 ).join(' '))
+
 require("gulp-cli")()
