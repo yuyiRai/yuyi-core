a79401e7e07c1440810883fd597018e798b34de2
yuyiRai <yuyi919606952@foxmail.com>
2020-05-07 14:44:19 +0800

    NEXT

--- packages/gulp-awesome/scripts/bin.js
+++ packages/gulp-awesome/scripts/bin.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 const colors = require('colors')
+const path = require('path')
 const args = process.argv.filter(i => i.indexOf('\\shelljs') === -1)
 if (args.length === 2) {
   args.push('-h')
 }
 
 // const cmd = `gulp --require @yuyi919/env/config/run-ts.js --cwd . ${args.join(' ')}`
-process.argv = args.concat(["--require", "@yuyi919/env/config/run-ts.js", "--cwd", process.cwd()])
+process.argv = args.concat(["--require", require.resolve("@yuyi919/env/config/run-ts.js"), "--cwd", process.cwd()])
+console.log(process.argv)
 console.log("gulp " + process.argv.slice(2).map(
   (arg, i, arr) => i + 4 >= arr.length ? colors.cyan(arg) : arg
 ).join(' '))
--- packages/ts-api-docs/bin.js
+++ packages/ts-api-docs/bin.js
@@ -2,6 +2,7 @@
 const { Args } = require('@yuyi919/utils/dist/NodeUtils');
 const { shell } = require('@yuyi919/shell-exec-awesome')
 const { execSync } = require('child_process')
+const crossEnv = require('cross-env')
 const { name: CLI_NAME, scripts } = require('./package.json')
 const fs = require('fs-extra')
 const path = require('path')
@@ -26,19 +27,16 @@ options.cwd = options.cwd.replace(/\\/g, '/')
 const { name: projectName } = require(`${options.cwd}/package.json`)
 console.log('yuyi-api-docs', options)
 
-
-const gulp = `cross-env `
-  + `CWD=${options.cwd} `
-  + `DOC_PROJECT_NAME=${projectName} `
-  + `DOC_MAIN_POINT=${options.dir} `
-  + `DOC_DEPENDS=${options.depends} `
-  + `yuyi-gulp --gulpfile ${resolve('./config/gulpfile.ts')}`
-  
+const cmd = `CWD=${options.cwd} ` +
+  `DOC_PROJECT_NAME=${projectName} ` +
+  `DOC_MAIN_POINT=${options.dir} ` +
+  `DOC_DEPENDS=${options.depends} ` +
+  `yuyi-gulp --gulpfile ${resolve('./config/gulpfile.ts')}`
+const cmds = cmd.split(' ')
 try {
-  const script = `${gulp}`
-  console.log(script)
-  const r = shell.exec(script)
-  console.log(r.output)
+  console.log(cmd)
+  crossEnv(cmds, {})
+
 } catch (error) {
   console.error(error)
   process.exit(0)
