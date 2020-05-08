21a8023f0a3c3e1ba387f8bddd115348de940f40
yuyiRai <yuyi919606952@foxmail.com>
2020-05-08 09:43:57 +0800

    r
    Signed-off-by: yuyiRai <yuyi919606952@foxmail.com>

--- packages/ts-api-docs/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./ts-api-docs.cjs.production.min.js')
-} else {
-  module.exports = require('./ts-api-docs.cjs.development.js')
-}
