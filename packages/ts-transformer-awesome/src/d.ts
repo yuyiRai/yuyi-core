690145f3e02761efc42b22d96156b4a94f55718a
yuyiRai <yuyi919606952@foxmail.com>
2020-06-15 10:35:40 +0800

    NEXT

--- packages/ts-transformer-awesome/src/d.ts
+++ packages/ts-transformer-awesome/src/d.ts
@@ -2,7 +2,6 @@ declare module 'ts-optchain/transform' {
   import ts from "typescript";
   export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile>
 }
-
 declare module 'require-context' {
   export default function require(dir: string, s: boolean, reg: RegExp): { keys(): string[] }
 }
