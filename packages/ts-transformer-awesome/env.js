7a398f45985093fc10fb5e6e5f3395928b09b736
yuyiRai <yuyi919606952@foxmail.com>
2020-11-17 07:54:39 +0800

    clean

--- packages/ts-transformer-awesome/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-global.filters = function () { return arguments[0]; };
-global.__DEV__ = process.env.NODE_ENV !== 'production';
-//# sourceMappingURL=env.js.map
--- packages/ts-transformer-awesome/tslib-extra.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const enum DecorateType {
-    nil = 0,
-    false = 1
-}
-export declare function __decorate_all<T extends Function, E extends {
-    decorators: Function[];
-    [key: string]: any;
-}>(keymap: Record<string, E>): (target: T) => T;
